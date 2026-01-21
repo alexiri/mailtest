8f1c5e9e0395897e2b33123d2f23c42af9dca9dc
Alex Iribarren <alex.iribarren@gmail.com>
2026-01-21 16:39:49 +0100

    Don't register twice

--- mailer/admin.py
+++ mailer/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from post_office.models import Email, EmailTemplate, Log
 
-admin.site.register(Email)
-admin.site.register(EmailTemplate)
-admin.site.register(Log)
+for model in (Email, EmailTemplate, Log):
+	if not admin.site.is_registered(model):
+		admin.site.register(model)
