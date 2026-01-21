7f88fa7bc7a235c1a9da89c84ce5623e17d6fa07
Alex Iribarren <alex.iribarren@gmail.com>
2026-01-21 18:08:00 +0100

    Use sdnotify

--- Dockerfile
+++ Dockerfile
@@ -14,6 +14,8 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . /app/
 
+RUN chmod +x /app/start.sh
+
 EXPOSE 8000
 
 CMD ["/app/start.sh"]
--- start.sh
+++ start.sh
@@ -3,4 +3,27 @@ set -e
 
 python manage.py migrate --noinput
 
-python manage.py runserver 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000 &
+server_pid=$!
+
+python - <<'PY'
+import socket
+import time
+from sdnotify import SystemdNotifier
+
+host = "127.0.0.1"
+port = 8000
+deadline = time.time() + 60
+
+while time.time() < deadline:
+	try:
+		with socket.create_connection((host, port), timeout=1):
+			SystemdNotifier().notify("READY=1")
+			break
+	except OSError:
+		time.sleep(0.5)
+else:
+	SystemdNotifier().notify("STATUS=Server failed to start")
+PY
+
+wait "$server_pid"
